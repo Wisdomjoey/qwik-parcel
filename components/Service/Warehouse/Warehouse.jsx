9ec31af42db1c216b15ee22df7cb58a32f061c9f
Jay Z <85133609+Wisdomjoey@users.noreply.github.com>
2025-03-22 17:50:34 +0100

    Update Warehouse.jsx

--- components/Service/Warehouse/Warehouse.jsx
+++ components/Service/Warehouse/Warehouse.jsx
@@ -1,1 +1,19 @@
+import "./Warehouse.css";
+import WH from '../../../assets/images/warehouse.jpg';
+import Container from "../Container/Container";
 
+export default function Haulage() {
+    return (
+        <div className="haulage">
+
+            <Container
+                img={ WH }
+                head='Warehouse Services'
+                txt="
+                    Our logistics warehousing service offers secure, scalable, and efficient storage solutions tailored to your business needs. We provide comprehensive inventory management, order fulfillment, and distribution services, ensuring your products are handled with care and delivered on time. With state-of-the-art facilities and advanced technology, we optimize your supply chain, reduce costs, and enhance customer satisfaction. Let us handle your warehousing needs, so you can focus on growing your business.
+                "
+            />
+            
+        </div>
+    )
+}
