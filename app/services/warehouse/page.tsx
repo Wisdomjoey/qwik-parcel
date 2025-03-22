6faebafe478e34edd54a73a3c4bb2e68678dbc80
Jay Z <85133609+Wisdomjoey@users.noreply.github.com>
2025-03-22 17:59:46 +0100

    Create page.tsx

---
+++ app/services/warehouse/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Navbar from "../../../components/Navbar/Navbar";
+import Warehouse from "../../../components/Service/Warehouse/Warehouse";
+
+export default function Support() {
+  return (
+    <>
+      <Navbar />
+      <Warehouse />
+    </>
+  );
+}
